cdc4bb6413fb98026d130a6fbac67667aae40c5a
Bruno <bruno.amorim032@gmail.com>
2021-02-17 00:40:59 -0300

    first commit

---
+++ zapbot.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+import time
+
+class WhatsappBot:
+    def __init__(self):
+        self.message = "bom dia amigo"
+        self.groups = ["TIRAR MUSICA"]
+        options = webdriver.ChromeOptions()
+        options.add_argument('lang=pt-br')
+        self.driver = webdriver.Chrome(executable_path=r'./chromedriver.exe')
+    
+    
+    def SendMessage(self):
+        #<span dir="auto" title="TIRAR MUSICA" class="_1hI5g _1XH7x _1VzZY">TIRAR MUSICA</span>
+        #<div tabindex="-1" class="DuUXI"><div tabindex="-1" class="_2HE1Z _1hRBM">
+        #<span data-testid="send" data-icon="send" class="">
+        self.driver.get('https://web.whatsapp.com')
+        # for group in self.groups:
+        for group in self.groups:
+            time.sleep(10)
+            group = self.driver.find_element_by_xpath(f"//span[@title='{group}']")
+            time.sleep(3)
+            group.click()
+            chat_box = self.driver.find_element_by_class_name('DuUXI')
+            time.sleep(3)
+            chat_box.click()
+            chat_box.send_keys(self.message)
+            send_button = self.driver.find_element_by_xpath(
+                '//span[@data-icon="send"]')
+            time.sleep(3)
+            send_button.click()
+            time.sleep(3)
+
+bot = WhatsappBot()
+bot.SendMessage()
