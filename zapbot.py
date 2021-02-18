e1cc6034dca6579773553bdcdec3e3f19435b145
Bruno <bruno.amorim032@gmail.com>
2021-02-18 12:03:59 -0300

    input interaction added

--- zapbot.py
+++ zapbot.py
@@ -3,8 +3,8 @@ import time
 
 class WhatsappBot:
     def __init__(self):
-        self.message = "bom dia amigo"
-        self.groups = ["TIRAR MUSICA"]
+        self.message = str(input('Digite a mensagem: '))
+        self.contact = str(input('Digite o contato: '))
         options = webdriver.ChromeOptions()
         options.add_argument('lang=pt-br')
         self.driver = webdriver.Chrome(executable_path=r'./chromedriver.exe')
@@ -16,22 +16,22 @@ class WhatsappBot:
         #<span data-testid="send" data-icon="send" class="">
         self.driver.get('https://web.whatsapp.com')
         # for group in self.groups:
-        for group in self.groups:
+        for group in self.contact:
             time.sleep(10)
-            group = self.driver.find_element_by_xpath(f"//span[@title='{group}']")
+            group = self.driver.find_element_by_xpath(f"//span[@title='{self.contact}']")
             time.sleep(3)
             group.click()
             chat_box = self.driver.find_element_by_class_name('DuUXI')
-            time.sleep(3)
+            time.sleep(2)
             chat_box.click()
             n = 0
             while n < 10:
                 chat_box.send_keys(self.message)
                 send_button = self.driver.find_element_by_xpath(
                     '//span[@data-icon="send"]')
-                time.sleep(3)
+                time.sleep(1)
                 send_button.click()
-                time.sleep(3)
+                time.sleep(1)
                 n += 1
 
 bot = WhatsappBot()
